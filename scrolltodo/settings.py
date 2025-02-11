7857e1c57ae9e6137bdeb325d051de3f51ea2fba
Shari Chung <sharichungdesign@gmail.com>
2025-02-11 03:13:36 +0000

    daphne listed before django

--- scrolltodo/settings.py
+++ scrolltodo/settings.py
@@ -40,6 +40,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'daphne',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -54,7 +55,6 @@ INSTALLED_APPS = [
     'allauth.socialaccount.providers.google',
     'allauth.socialaccount.providers.facebook',
     'channels',
-    'daphne',
 ]
 
 print("INSTALLED_APPS:", INSTALLED_APPS)
